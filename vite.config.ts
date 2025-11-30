c22920550d3ca0cba7792e3b237249a72a9d286d
Asuka109 <asuka109@foxmail.com>
2025-12-01 00:01:35 +0800

    refactor: update .env asset basename for improved path consistency and adjust Vite configuration to utilize new asset path

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { join } from 'node:path/posix';
 import { cloudflare } from '@cloudflare/vite-plugin';
 import { reactRouter } from '@react-router/dev/vite';
+import { withoutLeadingSlash } from 'ufo';
 import { defineConfig, loadEnv } from 'vite';
 import devtoolsJson from 'vite-plugin-devtools-json';
 import tsconfigPaths from 'vite-tsconfig-paths';
@@ -10,12 +10,9 @@ export default defineConfig(({ mode, isSsrBuild }) => {
   Object.assign(import.meta, { env });
   Object.assign(process.env, env);
 
-  const base = import.meta.env.VITE_APP_BASENAME || '/';
-
-  const assetsDir = join('assets', import.meta.env.VITE_ASSETS_BASENAME || '');
+  const assetsDir = withoutLeadingSlash(import.meta.env.VITE_ASSETS_BASENAME);
 
   return {
-    // base,
     build: { assetsDir },
     plugins: [
       cloudflare({ viteEnvironment: { name: 'ssr' } }),
